84961b310d764c4628ea6d1aa27644a149aa8972
Krzysiu <w61078@student.wsiz.rzeszow.pl>
2020-07-21 18:07:40 +0200

    Dodanie testów
    Testy jednostkowe
    
    Co-Authored-By: w61311 <64089163+w61311@users.noreply.github.com>
    Co-Authored-By: w61326 <67860052+w61326@users.noreply.github.com>

---
+++ KelnerPlus.Tests/ConnectionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KelnerPlus;
+
+namespace KelnerPlus.Tests
+{
+    [TestClass]
+    public class ConnectionsTests
+    {
+        [TestMethod]
+        public void TestGettingConnectionString()
+        {
+            string var = Connections.GetConnectionString();
+                Assert.IsNotNull(var);
+        }
+
+        [TestMethod]
+        public void TryConnect()
+        {
+
+            if (!Connections.IsServerConnected())
+                Assert.Fail("Nie ma połączenia");
+        }
+    }
+}
---
+++ KelnerPlus.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KelnerPlus.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KelnerPlus.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7495a6f1-4638-4dbe-a014-ba90784d50cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
